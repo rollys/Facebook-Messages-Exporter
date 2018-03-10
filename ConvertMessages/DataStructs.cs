d176eaf0366ebacf8563366ccdbe31201e5f27ff
Jan Grzybek <36562889+kolkil@users.noreply.github.com>
2018-03-10 21:02:55 +0100

    Some small changes

--- ConvertMessages/DataStructs.cs
+++ ConvertMessages/DataStructs.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using FBConversation;
 namespace DataStructs
 {
     struct sMessage
--- ConvertMessages/HtmlParse.cs
+++ ConvertMessages/HtmlParse.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Collections.Generic;
 using DataStructs;
 using MyFileParser;
