3f263f70a044f7300fa9301fb5ca7cfd41563997
Jan Grzybek <36562889+kolkil@users.noreply.github.com>
2018-03-11 21:25:38 +0100

    More small changes

--- ConvertMessages/MyFileParser.cs
+++ ConvertMessages/MyFileParser.cs
@@ -132,8 +132,8 @@ namespace MyFileParser
                         string towrite = "";
                         for (int k = 0; k < data.columnanames.Count; k++)
                         {
-                            if (k == 0) towrite += data.columnanames[k].Replace(";", "");
-                            else towrite += " ; " + data.columnanames[k].Replace(";", "");
+                            if (k == 0) towrite += data.columnanames[k].Replace(";", "").Replace('\n', ' ');
+                            else towrite += " ; " + data.columnanames[k].Replace(";", "").Replace('\n', ' ');
                         }
                     }
 
@@ -143,8 +143,8 @@ namespace MyFileParser
 
                         for (int k = 0; k < data.columnanames.Count; k++)
                         {
-                            if (k == 0) towrite += data.data[i][k].Replace(";", "");
-                            else towrite += " ; " + data.data[i][k].Replace(";", "");
+                            if (k == 0) towrite += data.data[i][k].Replace(";", "").Replace('\n', ' ');
+                            else towrite += " ; " + data.data[i][k].Replace(";", "").Replace('\n', ' ');
                         }
 
                         sw.WriteLine(towrite);
